2d200fc8eb07cdf2479c2e9104ace69bc273568a
naohito-T <naohito.tanaka0523@gmail.com>
2021-08-31 00:51:40 +0900

    webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,43 @@
+/** ここをproductionに書き換えればpro環境で設定ができる。*/
+/** cross-envまでやる必要なないだろうと思った。 */
 const MODE = 'development';
 
 const isSourceMap = MODE === 'development';
 
 module.exports = {
-  // モード値を production に設定すると最適化された状態で、
-  // development に設定するとソースマップ有効でJSファイルが出力される
   mode: MODE,
-  // entry point
   entry: `./src/js/index.js`,
-  // output
   output: {
-    // output dir
     path: `${__dirname}/public`,
-    // output file
-    filename: `app.js`
+    filename: `app.js`,
   },
   module: {
     rules: [
-      // Sassファイルの読み込みとコンパイル
       {
-        test: /\.scss/, // 対象となるファイルの拡張子
+        test: /\.scss/,
         use: [
-          // linkタグに出力する機能
-          "style-loader",
-          // CSSをバンドルするための機能
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
-              // オプションでCSS内のurl()メソッドの取り込みを禁止する
-              url: false,
-              // ソースマップの利用有無
+              url: true,
               sourceMap: isSourceMap,
-
-              // 0 => no loaders (default);
-              // 1 => postcss-loader;
-              // 2 => postcss-loader, sass-loader
-              importLoaders: 2
-            }
+              importLoaders: 2,
+            },
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
-              // ソースマップの利用有無
-              sourceMap: isSourceMap
+              sourceMap: isSourceMap,
             },
           },
         ],
       },
+      {
+        test: /\.(gif|png|jpg|svg)$/,
+        type: 'asset/inline',
+      },
     ],
   },
-  // ES5(IE11等)向けの指定（webpack 5以上で必要）
-  target: ["web", "es5"],
+  target: ['web', 'es5'],
 };
